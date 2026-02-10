bc56298ea69235a694aaf6951fa125b831108ee7
Ego Nwaekpe <glorynwaekpe@gmail.com>
2026-02-10 21:16:02 +0100

    Initial project setup

---
+++ src/domain/moneyFactory.ts
@@ -0,0 +1,4 @@
+import type { Money } from "../domain/money";
+import type { RoundingMode } from "../domain/roundingMode";
+
+export default function MoneyFactory(a: number, roundMode: RoundMode) {}
